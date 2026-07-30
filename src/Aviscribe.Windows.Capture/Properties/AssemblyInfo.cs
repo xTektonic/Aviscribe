737dff8c409713689b2e9b17f82402abb5735c92
Tektonic <xtektonicofficial@gmail.com>
2026-07-30 19:01:55 -0400

    Normalize project and folder names

--- src/Aviscribe.Windows.Capture/Properties/AssemblyInfo.cs
+++ src/Aviscribe.Windows.Capture/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Talkatoo.Windows.Capture")]
+[assembly: AssemblyTitle("Aviscribe.Windows.Capture")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Talkatoo.Windows.Capture")]
+[assembly: AssemblyProduct("Aviscribe.Windows.Capture")]
 [assembly: AssemblyCopyright("Copyright ©  2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
